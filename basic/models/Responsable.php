3b93a68d5ccb5d89ff50d19911cc4dd1bfa3b4bb
SamuelMoralesMoncada <smoralesmoncada@gmail.com>
2020-08-25 23:26:58 -0400

    ResponsableCRUD

---
+++ basic/models/Responsable.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "responsable".
+ *
+ * @property int $F_IDRESP
+ * @property string|null $F_NOMBRERESP
+ *
+ * @property Asigna[] $asignas
+ * @property Producto[] $fIDPRODs
+ */
+class Responsable extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'responsable';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['F_NOMBRERESP'], 'string', 'max' => 100],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'F_IDRESP' => 'F Idresp',
+            'F_NOMBRERESP' => 'F Nombreresp',
+        ];
+    }
+
+    /**
+     * Gets query for [[Asignas]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAsignas()
+    {
+        return $this->hasMany(Asigna::className(), ['F_IDRESP' => 'F_IDRESP']);
+    }
+
+    /**
+     * Gets query for [[FIDPRODs]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getFIDPRODs()
+    {
+        return $this->hasMany(Producto::className(), ['F_IDPROD' => 'F_IDPROD'])->viaTable('asigna', ['F_IDRESP' => 'F_IDRESP']);
+    }
+}
---
+++ basic/views/Responsable/index.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\ResponsableSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Responsables';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="responsable-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Responsable', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'F_IDRESP',
+            'F_NOMBRERESP',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+
+</div>
