ed705a342858a921da19fa36d69f95638aae5b3a
SamuelMoralesMoncada <smoralesmoncada@gmail.com>
2020-08-25 23:12:38 -0400

    conexión_bd

--- basic/config/db.php
+++ basic/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=inventario_cab',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
