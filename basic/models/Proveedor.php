0de683ad542a060af090affda34aaffe80482423
SamuelMoralesMoncada <smoralesmoncada@gmail.com>
2020-08-25 23:20:33 -0400

    ProveedorCRUD

---
+++ basic/models/Proveedor.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "proveedor".
+ *
+ * @property int $F_IDPROV
+ * @property string|null $F_NOMBREPROV
+ *
+ * @property Producto[] $productos
+ */
+class Proveedor extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'proveedor';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['F_NOMBREPROV'], 'string', 'max' => 100],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'F_IDPROV' => 'F Idprov',
+            'F_NOMBREPROV' => 'F Nombreprov',
+        ];
+    }
+
+    /**
+     * Gets query for [[Productos]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProductos()
+    {
+        return $this->hasMany(Producto::className(), ['F_IDPROV' => 'F_IDPROV']);
+    }
+}
---
+++ basic/views/Proveedor/_form.php
@@ -0,0 +1,23 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Proveedor */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="proveedor-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'F_NOMBREPROV')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
