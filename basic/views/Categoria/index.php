816eabb6f5b00a4152fb4a71a4706e73d20a8c25
SamuelMoralesMoncada <smoralesmoncada@gmail.com>
2020-08-30 21:40:22 -0400

    MejorasIndex

--- basic/views/Categoria/index.php
+++ basic/views/Categoria/index.php
@@ -24,7 +24,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+            //['class' => 'yii\grid\SerialColumn'],
 
             'F_IDCAT',
             'F_NOMBRECAT',
--- basic/views/Responsable/index.php
+++ basic/views/Responsable/index.php
@@ -24,7 +24,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+            //['class' => 'yii\grid\SerialColumn'],
 
             'F_IDRESP',
             'F_NOMBRERESP',
